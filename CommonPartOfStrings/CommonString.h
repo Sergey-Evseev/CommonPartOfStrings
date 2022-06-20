6a0e944b7c53dbc5c10714667fd8d1d849a6e2b3
Sergey Evseev <sevseev@gmail.com>
2022-06-20 17:40:26 +0300

    Comment about search in strings

--- CommonPartOfStrings/CommonString.h
+++ CommonPartOfStrings/CommonString.h
@@ -5,13 +5,14 @@
 #include <iterator>
 
 using namespace std;
-
+//Методы поиска в строке: Методичка 5 стр. 156
 class CommonString
 {
 public:
 	string GetCommonString(string const& str1, string const&str2) {
 		for (auto it = str1.begin(); it < str1.end(); ++it) {
-			if (str1.find(str2) < str1.length()) {				
+			if (str1.find(str2) < str1.length()) {	
+
 				cout << (str1 + str2);
 				return str1 + str2;
 			};
