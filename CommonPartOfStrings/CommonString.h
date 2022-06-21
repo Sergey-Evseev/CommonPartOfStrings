86c11164f6d8eff110c2ff3b2b6d76b57d906558
Sergey Evseev <sevseev@gmail.com>
2022-06-21 20:17:00 +0300

    if loop

--- CommonPartOfStrings/CommonString.h
+++ CommonPartOfStrings/CommonString.h
@@ -10,14 +10,15 @@ class CommonString
 {
 public:
 	string GetCommonString(string const& str1, string const&str2) {
-		for (auto it = str1.begin(); it < str1.end(); ++it) {
-			if (str1.find(str2) < str1.length()) {	
 
+		if (str1.find(str2) < str1.length()) {
+
+			for (auto it = str1.begin(); it < str1.end(); ++it) {
 				cout << (str1 + str2);
 				return str1 + str2;
 			};
-		}
-	};
+		} else { cout << "Строки не имеют совпадений" << endl; }
+	}
 	
 	void printString() {
 		
